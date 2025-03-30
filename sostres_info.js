95ba3ddcba7c85cb25afc30af4653f090d295cce
Pere Gilabert Roca <perecasxiru@hotmail.com>
2025-03-30 21:09:11 +0200

    sostres

---
+++ sostres_info.js
@@ -0,0 +1,339 @@
+var json_sostres = [
+    {
+        "nom": "Pica d'Estats",
+        "alt": 3144,
+        "lat": 42.6669371771,
+        "lon": 1.39790065859,
+        "completat": true,
+        "comarca": "Pallars Sobirà"
+    },
+    {
+        "nom": "Pic de Comaloforno",
+        "alt": 3029,
+        "lat": 42.5913820355,
+        "lon": 0.827834271065,
+        "completat": false,
+        "comarca": "Alta Ribagorça"
+    },
+    {
+        "nom": "Tuc de Molières",
+        "alt": 3010,
+        "lat": 42.6294790552,
+        "lon": 0.698560841901,
+        "completat": true,
+        "comarca": "Val d'Aran"
+    },
+    {
+        "nom": "Pic de Peguera",
+        "alt": 2983,
+        "lat": 42.540541256,
+        "lon": 1.01195828499,
+        "completat": false,
+        "comarca": "Pallars Jussà"
+    },
+    {
+        "nom": "Puigpedrós",
+        "alt": 2915,
+        "lat": 42.4875750896,
+        "lon": 1.76193328898,
+        "completat": true,
+        "comarca": "Baixa Cerdanya",
+    },
+    {
+        "nom": "Puigmal",
+        "alt": 2910,
+        "lat": 42.3832660904,
+        "lon": 2.11679066241,
+        "completat": true,
+        "comarca": "Ripollès"
+    },
+    {
+        "nom": "Pic de Salòria",
+        "alt": 2789,
+        "lat": 42.5144490848,
+        "lon": 1.38513757735,
+        "completat": false,
+        "comarca": "Alt Urgell"
+    },
+    {
+        "nom": "Pic de Costa Cabirolera",
+        "alt": 2604,
+        "lat": 42.281459,
+        "lon": 1.670418,
+        "completat": true,
+        "comarca": "Berguedà"
+    },
+    {
+        "nom": "Pedró dels Quatre Batlles",
+        "alt": 2387,
+        "lat": 42.1847955583,
+        "lon": 1.5209102343,
+        "completat": false,
+        "comarca": "Solsonès"
+    },
+    {
+        "nom": "Turó de l'Home",
+        "alt": 1706,
+        "lat": 41.776488,
+        "lon": 2.434813,
+        "completat": true,
+        "comarca": "Vallès Oriental"
+    },
+    {
+        "nom": "Les Agudes",
+        "alt": 1705,
+        "lat": 41.7893335256,
+        "lon": 2.44395480371,
+        "completat": true,
+        "comarca": "La Selva"
+    },
+    {
+        "nom": "Matagalls",
+        "alt": 1697,
+        "lat": 41.8088174884,
+        "lon": 2.38261778463,
+        "completat": true,
+        "comarca": "Osona"
+    },
+    {
+        "nom": "Tossal de les Torretes",
+        "alt": 1676,
+        "lat": 42.021986,
+        "lon": 0.938342,
+        "completat": false,
+        "comarca": "Noguera"
+    },
+    {
+        "nom": "Comanegra",
+        "alt": 1557,
+        "lat": 42.3333072525,
+        "lon": 2.52776092867,
+        "completat": false,
+        "comarca": "Garrotxa"
+    },
+    {
+        "nom": "Roc del Comptador",
+        "alt": 1451,
+        "lat": 42.4227911919,
+        "lon": 2.72729098521,
+        "completat": false,
+        "comarca": "Gironès"
+    },
+    {
+        "nom": "Caro",
+        "alt": 1441,
+        "lat": 40.8031301137,
+        "lon": 0.343121075505,
+        "completat": false,
+        "comarca": "Baix Ebre"
+    },
+    {
+        "nom": "Tossal dels Tres Reis",
+        "alt": 1350,
+        "lat": 40.732788284,
+        "lon": 0.170231508703,
+        "completat": false,
+        "comarca": "Montsià"
+    },
+    {
+        "nom": "Sant Jeroni",
+        "alt": 1236,
+        "lat": 41.6053835626,
+        "lon": 1.81150454088,
+        "completat": true,
+        "comarca": "Anoia / Bages"
+    },
+    {
+        "nom": "Tosseta Rasa",
+        "alt": 1217,
+        "lat": 40.845019,
+        "lon": 0.327442,
+        "completat": false,
+        "comarca": "Terra Alta"
+    },
+    {
+        "nom": "Puigdon",
+        "alt": 1206,
+        "lat": 42.143111,
+        "lon": 2.092911,
+        "completat": true,
+        "comarca": "Lluçanès"
+    },
+    {
+        "nom": "Tossal de la Baltasana",
+        "alt": 1201,
+        "lat": 41.3260040526,
+        "lon": 1.00419364629,
+        "completat": false,
+        "comarca": "Baix Camp / Conca de Barberà"
+    },
+    {
+        "nom": "L'Albarda Castellana",
+        "alt": 1178,
+        "lat": 41.60102,
+        "lon": 1.813379,
+        "completat": false,
+        "comarca": "Baix Llobregat"
+    },
+    {
+        "nom": "Roca Corbatera",
+        "alt": 1163,
+        "lat": 41.288591728,
+        "lon": 0.891232362067,
+        "completat": false,
+        "comarca": "Priorat"
+    },
+    {
+        "nom": "La Mola de Sant Llorenç del Munt",
+        "alt": 1102,
+        "lat": 41.6411519036,
+        "lon": 2.01767220976,
+        "completat": true,
+        "comarca": "Vallès Occidental"
+    },
+    {
+        "nom": "Serrat de Sant Joan",
+        "alt": 1069,
+        "lat": 41.8456,
+        "lon": 2.145608,
+        "completat": true,
+        "comarca": "Moianès"
+    },
+    {
+        "nom": "Punta del Curull",
+        "alt": 1022,
+        "lat": 41.3552988479,
+        "lon": 0.96432912146,
+        "completat": false,
+        "comarca": "Garrigues"
+    },
+    {
+        "nom": "Roca de Migdia",
+        "alt": 1022,
+        "lat": 41.292531,
+        "lon": 1.061511,
+        "completat": false,
+        "comarca": "Alt Camp"
+    },
+    {
+        "nom": "Puigsou o Rocacorba",
+        "alt": 991,
+        "lat": 42.072481,
+        "lon": 2.686111,
+        "completat": true,
+        "comarca": "Gironès"
+    },
+    {
+        "nom": "Creu de Santos",
+        "alt": 942,
+        "lat": 40.9406887493,
+        "lon": 0.586018857692,
+        "completat": false,
+        "comarca": "Ribera d'Ebre"
+    },
+    {
+        "nom": "Comaestremer",
+        "alt": 886,
+        "lat": 42.08325,
+        "lon": 2.682389,
+        "completat": false,
+        "comarca": "Pla de l'Estany"
+    },
+    {
+        "nom": "Talaia del Montmell",
+        "alt": 861,
+        "lat": 41.3357901533,
+        "lon": 1.46671424676,
+        "completat": false,
+        "comarca": "Baix Penedès"
+    },
+    {
+        "nom": "Turó de Galutxo",
+        "alt": 851,
+        "lat": 41.555125,
+        "lon": 1.380869,
+        "completat": false,
+        "comarca": "Segarra"
+    },
+    {
+        "nom": "Puig de les Agulles",
+        "alt": 841,
+        "lat": 41.436028,
+        "lon": 1.523361,
+        "completat": false,
+        "comarca": "Alt Penedès"
+    },
+    {
+        "nom": "Tossal Gros de Vallbona",
+        "alt": 803,
+        "lat": 41.4688603287,
+        "lon": 1.11287099172,
+        "completat": false,
+        "comarca": "Urgell"
+    },
+    {
+        "nom": "Turó d'en Vives",
+        "alt": 766,
+        "lat": 41.659222,
+        "lon": 2.555194,
+        "completat": false,
+        "comarca": "Maresme"
+    },
+    {
+        "nom": "Puig de la Mola",
+        "alt": 534,
+        "lat": 41.319323,
+        "lon": 1.847911,
+        "completat": false,
+        "comarca": "Garraf"
+    },
+    {
+        "nom": "Puig d'Aiguabona",
+        "alt": 533,
+        "lat": 41.887333,
+        "lon": 2.990389,
+        "completat": false,
+        "comarca": "Baix Empordà"
+    },
+    {
+        "nom": "Tibidabo",
+        "alt": 516,
+        "lat": 41.4225,
+        "lon": 2.118611,
+        "completat": false,
+        "comarca": "Baix Empordà"
+    },
+    {
+        "nom": "Puntal dels Escambrons",
+        "alt": 500,
+        "lat": 41.276058,
+        "lon": 0.432656,
+        "completat": false,
+        "comarca": "Segrià"
+    },
+    {
+        "nom": "La Mola",
+        "alt": 317,
+        "lat": 41.211622,
+        "lon": 1.417321,
+        "completat": false,
+        "comarca": "Tarragonès"
+    },
+    {
+        "nom": "Tossal de l'Infern",
+        "alt": 301,
+        "lat": 41.594433,
+        "lon": 0.869908,
+        "completat": false,
+        "comarca": "Pla d'Urgell"
+    },
+
+
+
+
+
+
+
+
+
+];
